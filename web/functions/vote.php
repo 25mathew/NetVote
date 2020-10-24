81ca32c02434de30fab8a5876190e66d35292b43
Devon Nelson <s809569@sbuniv.edu>
2020-10-24 11:59:42 -0500

    form action

--- web/functions/vote.php
+++ web/functions/vote.php
@@ -1,6 +1,6 @@
 <?php
 	require_once 'sql.php';
-	if(isset($_POST[''] && isset($_POST['']){ //username and password 
+	if(isset($_POST['username'] && isset($_POST['password']){ //username and password 
 		
 		
 	}
