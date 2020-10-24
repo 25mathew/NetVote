ba4df5b0975f4b7db3c75f294be4fa7f87984734
Mathew Baldinger <s785721@sbuniv.edu>
2020-10-23 23:56:13 -0500

    commit

--- web/index.php
+++ web/index.php
@@ -3,14 +3,15 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
-    <p>First paragraph ever</p>
-    <p>Second paragraph ever</p>
-    <p>Third paragraph ever</p>
     <title>NetVote</title>
 </head>
 <body>
-<a href="https://codepen.io/ARS" target="_blank"><span>Register</span></a>
 
-<a href="https://codepen.io/ARS" target="_blank"><span>Vote</span></a>
+    <p>First paragraph ever</p>
+    <p>Second paragraph ever</p>
+    <p>Third paragraph ever</p>
+
+    <a href="https://codepen.io/ARS" target="_blank"><span>Register</span></a>
+    <a href="https://codepen.io/ARS" target="_blank"><span>Vote</span></a>
 </body>
 </html>
